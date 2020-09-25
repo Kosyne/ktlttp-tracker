f801370d44cfd4be530bd0cdf1fc0eea112a62dc
Bonta-kun <40473493+Bonta0@users.noreply.github.com>
2020-09-25 04:12:46 +0200

    Autotracker: incremental keys + some cleanup

--- public/script/autotrcker.js
+++ public/script/autotrcker.js
@@ -241,193 +241,34 @@ function autotrackDoTracking(data) {
     updatechest(62, 0x300, 0x40); // Pedestal
     updatechest_group(63, [[0x228, 0x10], [0x228, 0x20]]); // Waterfall Fairy Left + Right
 
-
-    //if (newbit_group([[0x150, 0x10], [0x152, 0x10], [0x172, 0x10], [0x170, 0x10], [0x154, 0x10], [0x191, 0x08]])) { // Eastern Palace - Compass + Big Chest + Cannonball + Big Key Chest + Map + Boss
-    //    rootRef.child('dungeonchests').child(0).set(0);
-    //    rootRef.child('items').child("chest0").set(0);
-    //}
-
-    if (newbit(0x191, 0x08)) { // Eastern Palace Boss
-        rootRef.child('dungeonbeaten').child(0).set(true);
-        rootRef.child('items').child("boss0").set(2);
-    }
-
-
-    //if (newbit_group([[0x0E6, 0x10], [0x0E7, 0x04], [0x0E8, 0x10], [0x10A, 0x10], [0x0EA, 0x10], [0x067, 0x08]])) { // Desert Palace - Big Chest + Torch + Map Chest + Compass Chest + Big Key Chest + Boss
-    //    rootRef.child('dungeonchests').child(1).set(0);
-    //    rootRef.child('items').child("chest1").set(0);
-    //}
-
-    if (newbit(0x067, 0x08)) { // Desert Palace Boss
-        rootRef.child('dungeonbeaten').child(1).set(true);
-        rootRef.child('items').child("boss1").set(2);
-    }
-
-
-    //if (newbit_group([[0x10F, 0x04], [0x0EE, 0x10], [0x10E, 0x10], [0x04E, 0x20], [0x04E, 0x10], [0x00F, 0x08]])) { // Hera - Cage + Map Chest + Big Key Chest + Compass Chest + Big Chest + Boss
-    //    rootRef.child('dungeonchests').child(2).set(0);
-    //    rootRef.child('items').child("chest2").set(0);
-    //}
-
-    if (newbit(0x00F, 0x08)) { // Hera Boss
-        rootRef.child('dungeonbeaten').child(2).set(true);
-        rootRef.child('items').child("boss2").set(2);
-    }
-
-
-    //if (newbit_group([[0x1C0, 0x10], [0x1A0, 0x10]])) // Castle Tower
-    //    rootRef.child('dungeonchests').child(xx).set(0);
+    function update_boss(boss, offset) {
+        if (newbit(offset, 0x08)) { // Eastern Palace Boss
+            rootRef.child('dungeonbeaten').child(boss).set(true);
+            rootRef.child('items').child("boss" + boss.toString()).set(2);
+        }
+    };
+    update_boss(0, 0x191); // Eastern Palace
+    update_boss(1, 0x067); // Desert Palace
+    update_boss(2, 0x00F); // Hera
+    update_boss(3, 0x0B5); // Palace of Darkness
+    update_boss(4, 0x00D); // Swamp Palace
+    update_boss(5, 0x053); // Skull Woods
+    update_boss(6, 0x159); // Thieves Town
+    update_boss(7, 0x1BD); // Ice Palace
+    update_boss(8, 0x121); // Misery Mire
+    update_boss(9, 0x149); // Turtle Rock
+    update_boss(10, 0x01B); // Ganons Tower
 
     if (changed(0x3C5) && data[0x3C5] >= 3) // Agahnim Killed
         rootRef.child('items').child("agahnim").set(1);
 
-/*
-                  'Palace of Darkness - Shooter Room': (0x012, 0x10),
-                  'Palace of Darkness - The Arena - Bridge': (0x054, 0x20),
-                  'Palace of Darkness - Stalfos Basement': (0x014, 0x10),
-                  'Palace of Darkness - Big Key Chest': (0x074, 0x10),
-                  'Palace of Darkness - The Arena - Ledge': (0x054, 0x10),
-                  'Palace of Darkness - Map Chest': (0x056, 0x10),
-                  'Palace of Darkness - Compass Chest': (0x034, 0x20),
-                  'Palace of Darkness - Dark Basement - Left': (0x0D4, 0x10),
-                  'Palace of Darkness - Dark Basement - Right': (0x0D4, 0x20),
-                  'Palace of Darkness - Dark Maze - Top': (0x032, 0x10),
-                  'Palace of Darkness - Dark Maze - Bottom': (0x032, 0x20),
-                  'Palace of Darkness - Big Chest': (0x034, 0x10),
-                  'Palace of Darkness - Harmless Hellway': (0x034, 0x40),
-*/
-    if (newbit(0x0B5, 0x08)) { // Palace of Darkness Boss
-        rootRef.child('dungeonbeaten').child(3).set(true);
-        rootRef.child('items').child("boss3").set(2);
-    }
-
-/*
-                  'Swamp Palace - Entrance': (0x050, 0x10),
-                  'Swamp Palace - Map Chest': (0x06E, 0x10),
-                  'Swamp Palace - Big Chest': (0x06C, 0x10),
-                  'Swamp Palace - Compass Chest': (0x08C, 0x10),
-                  'Swamp Palace - Big Key Chest': (0x06A, 0x10),
-                  'Swamp Palace - West Chest': (0x068, 0x10),
-                  'Swamp Palace - Flooded Room - Left': (0x0EC, 0x10),
-                  'Swamp Palace - Flooded Room - Right': (0x0EC, 0x20),
-                  'Swamp Palace - Waterfall Room': (0x0CC, 0x10),
-*/
-    if (newbit(0x00D, 0x08)) { // Swamp Palace Boss
-        rootRef.child('dungeonbeaten').child(4).set(true);
-        rootRef.child('items').child("boss4").set(2);
-    }
-
-/*
-                  'Skull Woods - Compass Chest': (0x0CE, 0x10),
-                  'Skull Woods - Map Chest': (0x0B0, 0x20),
-                  'Skull Woods - Big Chest': (0x0B0, 0x10),
-                  'Skull Woods - Pot Prison': (0x0AE, 0x20),
-                  'Skull Woods - Pinball Room': (0x0D0, 0x10),
-                  'Skull Woods - Big Key Chest': (0x0AE, 0x10),
-                  'Skull Woods - Bridge Room': (0x0B2, 0x10),
-*/
-    if (newbit(0x053, 0x08)) { // Skull Woods Boss
-        rootRef.child('dungeonbeaten').child(5).set(true);
-        rootRef.child('items').child("boss5").set(2);
-    }
-
-/*
-                  'Thieves\' Town - Big Key Chest': (0x1B6, 0x20),
-                  'Thieves\' Town - Map Chest': (0x1B6, 0x10),
-                  'Thieves\' Town - Compass Chest': (0x1B8, 0x10),
-                  'Thieves\' Town - Ambush Chest': (0x196, 0x10),
-                  'Thieves\' Town - Attic': (0x0CA, 0x10),
-                  'Thieves\' Town - Big Chest': (0x088, 0x10),
-                  'Thieves\' Town - Blind\'s Cell': (0x08A, 0x10),
-*/
-    if (newbit(0x159, 0x08)) { // Thieves Town Boss
-        rootRef.child('dungeonbeaten').child(6).set(true);
-        rootRef.child('items').child("boss6").set(2);
-    }
-
-/*
-                  'Ice Palace - Compass Chest': (0x05C, 0x10),
-                  'Ice Palace - Freezor Chest': (0x0FC, 0x10),
-                  'Ice Palace - Big Chest': (0x13C, 0x10),
-                  'Ice Palace - Iced T Room': (0x15C, 0x10),
-                  'Ice Palace - Spike Room': (0x0BE, 0x10),
-                  'Ice Palace - Big Key Chest': (0x03E, 0x10),
-                  'Ice Palace - Map Chest': (0x07E, 0x10),
-*/
-    if (newbit(0x1BD, 0x08)) { // Ice Palace Boss
-        rootRef.child('dungeonbeaten').child(7).set(true);
-        rootRef.child('items').child("boss7").set(2);
-    }
-
-/*
-                  'Misery Mire - Big Chest': (0x186, 0x10),
-                  'Misery Mire - Map Chest': (0x186, 0x20),
-                  'Misery Mire - Main Lobby': (0x184, 0x10),
-                  'Misery Mire - Bridge Chest': (0x144, 0x10),
-                  'Misery Mire - Spike Chest': (0x166, 0x10),
-                  'Misery Mire - Compass Chest': (0x182, 0x10),
-                  'Misery Mire - Big Key Chest': (0x1A2, 0x10),
-*/
-    if (newbit(0x121, 0x08)) { // Misery Mire Boss
-        rootRef.child('dungeonbeaten').child(8).set(true);
-        rootRef.child('items').child("boss8").set(2);
-    }
-
-/*
-                  'Turtle Rock - Compass Chest': (0x1AC, 0x10),
-                  'Turtle Rock - Roller Room - Left': (0x16E, 0x10),
-                  'Turtle Rock - Roller Room - Right': (0x16E, 0x20),
-                  'Turtle Rock - Chain Chomps': (0x16C, 0x10),
-                  'Turtle Rock - Big Key Chest': (0x028, 0x10),
-                  'Turtle Rock - Big Chest': (0x048, 0x10),
-                  'Turtle Rock - Crystaroller Room': (0x008, 0x10),
-                  'Turtle Rock - Eye Bridge - Bottom Left': (0x1AA, 0x80),
-                  'Turtle Rock - Eye Bridge - Bottom Right': (0x1AA, 0x40),
-                  'Turtle Rock - Eye Bridge - Top Left': (0x1AA, 0x20),
-                  'Turtle Rock - Eye Bridge - Top Right': (0x1AA, 0x10),
-*/
-    if (newbit(0x149, 0x08)) { // Turtle Rock Boss
-        rootRef.child('dungeonbeaten').child(9).set(true);
-        rootRef.child('items').child("boss9").set(2);
-    }
-
-/*
-                  'Ganons Tower - Bob\'s Torch': (0x119, 0x04),
-                  'Ganons Tower - Hope Room - Left': (0x118, 0x20),
-                  'Ganons Tower - Hope Room - Right': (0x118, 0x40),
-                  'Ganons Tower - Tile Room': (0x11A, 0x10),
-                  'Ganons Tower - Compass Room - Top Left': (0x13A, 0x10),
-                  'Ganons Tower - Compass Room - Top Right': (0x13A, 0x20),
-                  'Ganons Tower - Compass Room - Bottom Left': (0x13A, 0x40),
-                  'Ganons Tower - Compass Room - Bottom Right': (0x13A, 0x80),
-                  'Ganons Tower - DMs Room - Top Left': (0x0F6, 0x10),
-                  'Ganons Tower - DMs Room - Top Right': (0x0F6, 0x20),
-                  'Ganons Tower - DMs Room - Bottom Left': (0x0F6, 0x40),
-                  'Ganons Tower - DMs Room - Bottom Right': (0x0F6, 0x80),
-                  'Ganons Tower - Map Chest': (0x116, 0x10),
-                  'Ganons Tower - Firesnake Room': (0x0FA, 0x10),
-                  'Ganons Tower - Randomizer Room - Top Left': (0x0F8, 0x10),
-                  'Ganons Tower - Randomizer Room - Top Right': (0x0F8, 0x20),
-                  'Ganons Tower - Randomizer Room - Bottom Left': (0x0F8, 0x40),
-                  'Ganons Tower - Randomizer Room - Bottom Right': (0x0F8, 0x80),
-                  'Ganons Tower - Bob\'s Chest': (0x118, 0x80),
-                  'Ganons Tower - Big Chest': (0x118, 0x10),
-                  'Ganons Tower - Big Key Room - Left': (0x038, 0x20),
-                  'Ganons Tower - Big Key Room - Right': (0x038, 0x40),
-                  'Ganons Tower - Big Key Chest': (0x038, 0x10),
-                  'Ganons Tower - Mini Helmasaur Room - Left': (0x07A, 0x10),
-                  'Ganons Tower - Mini Helmasaur Room - Right': (0x07A, 0x20),
-                  'Ganons Tower - Pre-Moldorm Chest': (0x07A, 0x40),
-                  'Ganons Tower - Validation Chest': (0x09A, 0x10)
-*/
-    if (newbit(0x01B, 0x08)) { // Ganons Tower Boss
-        rootRef.child('dungeonbeaten').child(10).set(true);
-        rootRef.child('items').child("boss10").set(2);
-    }
-
     function updatesmallkeys(dungeon, offset) {
-        if (changed(offset))
-            rootRef.child('smallkeys').child(dungeon).set(data[offset]);
+        if (changed(offset)) {
+            var newkeys = autotrackPrevData === null ? data[offset] : (data[offset] - autotrackPrevData[offset] + trackerData.smallkeys[dungeon]);
+            if (newkeys > trackerData.smallkeys[dungeon]) {
+                rootRef.child('smallkeys').child(dungeon).set(newkeys);
+            }
+        }
     };
     //updatesmallkeys(xx, 0x37C); updatesmallkeys(xx, 0x37D); // Escape
     updatesmallkeys(0, 0x37E);
